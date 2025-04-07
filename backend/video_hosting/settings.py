688340f7e44369118bc0d5c6e80e113373b049b6
Artemii <artem.vereteno.123@gmail.com>
2025-04-07 17:54:19 +0700

    yapizdaty

--- backend/video_hosting/settings.py
+++ backend/video_hosting/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'video_hosting_db',
         'USER': 'postgres',  # твой пользователь
-        'PASSWORD': 'postgres',  # твой пароль
+        'PASSWORD': '123',  # твой пароль
         'HOST': 'localhost',
         'PORT': '5432',
     }
